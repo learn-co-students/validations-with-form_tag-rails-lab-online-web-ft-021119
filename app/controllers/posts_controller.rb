9b214a619db953e2c5ce66c4bd90b6ce73b4e938
kirstenwerner <kirstenmwerner@gmail.com>
2019-04-24 15:27:49 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,9 +9,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    post = Post.new(post_params)
-    if post.valid?
-      @post.update(post_params)
+    if @post.update(post_params)
       redirect_to post_path(@post)
     else
       render :edit
