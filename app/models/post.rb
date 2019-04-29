b1a3dc81b9ed692ececfde7788861f16ea67ed8e
TaylorSwinton <taylorswinton1@gmail.com>
2019-04-29 18:42:08 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+    #title is not blank
+    #content is min 100
+    #category is fiction or non-fiction
+
+    validates :title, presence: true
+    validates :content , length: { minimum: 100 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
 end
